8cd37221acafdabd1e3925c88cc24142bf51ceb3
Omja Das <o.das@student.unimelb.edu.au>
2018-04-16 21:11:03 +1000

    added error checking if no port provided

--- server.c
+++ server.c
@@ -1,6 +1,13 @@
 #include <pthread.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <sys/socket.h>
 
 int main(int argc, char const *argv[]) {
+    int sockfd;
+    if (argc < 2) {
+        fprintf(stderr, "ERROR, no port provided\n");
+        exit(1);
+    }
     return 0;
 }
