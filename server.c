ba7526dba7f31db23c49adf8a9b6aa8d2a3a932c
Omja Das <o.das@student.unimelb.edu.au>
2018-04-16 21:45:03 +1000

    added socket creation, binding, listening, and connection establishment

--- server.c
+++ server.c
@@ -1,13 +1,63 @@
+#include <netinet/in.h>
 #include <pthread.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <sys/socket.h>
 
 int main(int argc, char const *argv[]) {
-    int sockfd;
-    if (argc < 2) {
+    int sockfd, newsockfd, portno;
+    struct sockaddr_in serv_addr, cli_addr;
+    socklen_t clilen;
+
+    if (argc < 3) {
         fprintf(stderr, "ERROR, no port provided\n");
         exit(1);
     }
+
+    /* Create TCP socket */
+
+    sockfd = socket(AF_INET, SOCK_STREAM, 0);
+
+    if (sockfd < 0) {
+        perror("ERROR opening socket");
+        exit(1);
+    }
+
+    bzero((char *)&serv_addr, sizeof(serv_addr));
+
+    portno = atoi(argv[1]);
+
+    /* Create address we're going to listen on (given port number)
+     - converted to network byte order & any IP address for
+     this machine */
+
+    serv_addr.sin_family = AF_INET;
+    serv_addr.sin_addr.s_addr = INADDR_ANY;
+    serv_addr.sin_port = htons(portno); // store in machine-neutral format
+
+    /* Bind address to the socket */
+
+    if (bind(sockfd, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0) {
+        perror("ERROR on binding");
+        exit(1);
+    }
+
+    /* Listen on socket - means we're ready to accept connections -
+     incoming connection requests will be queued */
+
+    listen(sockfd, 5);
+
+    clilen = sizeof(cli_addr);
+
+    /* Accept a connection - block until a connection is ready to
+     be accepted. Get back a new file descriptor to communicate on. */
+
+    newsockfd = accept(sockfd, (struct sockaddr *)&cli_addr, &clilen);
+
+    if (newsockfd < 0) {
+        perror("ERROR on accept");
+        exit(1);
+    }
+
     return 0;
 }
