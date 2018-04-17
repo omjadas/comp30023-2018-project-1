e38575b8a60eb70e0a1acb81bb729abbe96940a8
Omja Das <o.das@student.unimelb.edu.au>
2018-04-18 00:52:44 +1000

    initial sequential implementation

--- server.c
+++ server.c
@@ -9,9 +9,12 @@
 #define BUFFER_SIZE 2048
 
 int read_file(char **, char *);
+char *get_dir(char *);
+const char *mime_type(char *);
+void send_file(char *, int, int);
 
 int main(int argc, char *argv[]) {
-    int sockfd, newsockfd, portno;
+    int sockfd, portno;
     struct sockaddr_in serv_addr, cli_addr;
     char *root_dir;
     char request_buffer[BUFFER_SIZE];
@@ -42,7 +45,7 @@ int main(int argc, char *argv[]) {
      this machine */
 
     serv_addr.sin_family = AF_INET;
-    serv_addr.sin_addr.s_addr = INADDR_ANY;
+    serv_addr.sin_addr.s_addr = htonl(INADDR_ANY);
     serv_addr.sin_port = htons(portno); // store in machine-neutral format
 
     /* Bind address to the socket */
@@ -52,34 +55,61 @@ int main(int argc, char *argv[]) {
         exit(1);
     }
 
-    /* Listen on socket - means we're ready to accept connections -
-     incoming connection requests will be queued */
-
-    listen(sockfd, 10);
-
     clilen = sizeof(cli_addr);
 
-    /* Accept a connection - block until a connection is ready to
-     be accepted. Get back a new file descriptor to communicate on. */
+    while (1) {
+        int newsockfd;
+        /* Listen on socket - means we're ready to accept connections -
+         incoming connection requests will be queued */
 
-    newsockfd = accept(sockfd, (struct sockaddr *)&cli_addr, &clilen);
+        listen(sockfd, 1);
 
-    if (newsockfd < 0) {
-        perror("ERROR on accept");
-        exit(1);
+        /* Accept a connection - block until a connection is ready to
+         be accepted. Get back a new file descriptor to communicate on. */
+
+        newsockfd = accept(sockfd, (struct sockaddr *)&cli_addr, &clilen);
+
+        if (newsockfd < 0) {
+            perror("ERROR on accept");
+            exit(1);
+        }
+
+        memset(request_buffer, '0', BUFFER_SIZE);
+
+        read(newsockfd, request_buffer, BUFFER_SIZE);
+
+        // printf("%s\n", request_buffer);
+
+        char *file_path = get_dir(request_buffer);
+
+        char full_path[BUFFER_SIZE * 2];
+
+        strcpy(full_path, root_dir);
+        strcat(full_path, file_path);
+
+        char *file;
+        int file_len;
+
+        file_len = read_file(&file, full_path);
+
+        if (file_len >= 0) {
+            printf("200 OK\n\n");
+            char header[BUFFER_SIZE * 4];
+            strcat(header, "HTTP/1.0 200 OK\r\n");
+            strcat(header, mime_type(full_path));
+            write(newsockfd, header, strlen(header));
+            write(newsockfd, file, file_len);
+            printf("%s", header);
+            // write(newsockfd, "\r\n", 2);
+            free(file);
+        } else {
+            printf("404 Not Found\n\n");
+            write(newsockfd, "HTTP/1.0 404 Not Found\r\n", 25);
+        }
+
+        close(newsockfd);
+        free(file_path);
     }
-
-    memset(request_buffer, '0', BUFFER_SIZE);
-
-    read(newsockfd, request_buffer, BUFFER_SIZE);
-
-    char file_path[BUFFER_SIZE];
-    int i;
-    for (i = 4; request_buffer[i] != ' '; i++) {
-        file_path[i - 4] = request_buffer[i];
-    }
-
-    printf("%s%s\n", root_dir, file_path);
 
     /* close socket */
 
@@ -91,13 +121,48 @@ int read_file(char **output, char *path) {
     FILE *file_ptr;
     long file_len;
 
-    file_ptr = fopen(path, "rb");
-    fseek(file_ptr, 0, SEEK_END);
-    file_len = ftell(file_ptr);
-    rewind(file_ptr);
+    // printf("%s\n", path);
 
-    *output = (char *)malloc((file_len + 1) * sizeof(char));
-    fread(*output, file_len, 1, file_ptr);
-    fclose(file_ptr);
-    return file_len;
+    if (file_ptr = fopen(path, "rb")) {
+        fseek(file_ptr, 0, SEEK_END);
+        file_len = ftell(file_ptr);
+        rewind(file_ptr);
+
+        *output = (char *)malloc((file_len + 1) * sizeof(char));
+        fread(*output, file_len, 1, file_ptr);
+        fclose(file_ptr);
+        return file_len;
+    }
+    return -1;
+}
+
+char *get_dir(char *request) {
+
+    // printf("\n%s\n", request);
+
+    char *path = malloc(BUFFER_SIZE * sizeof(char));
+    int i;
+    for (i = 4; request[i] != ' '; i++) {
+        path[i - 4] = request[i];
+    }
+    path[i - 4] = '\0';
+    return path;
+}
+
+const char *mime_type(char *path) {
+    char *dot = strrchr(path, '.');
+    dot++;
+    if (strcmp(dot, "html") == 0) {
+        return "Content-Type: text/html\r\n\r\n";
+    } else if (strcmp(dot, "jpg") == 0) {
+        return "Content-Type: image/jpeg\r\n\r\n";
+    } else if (strcmp(dot, "css") == 0) {
+        return "Content-Type: text/css\r\n\r\n";
+    } else if (strcmp(dot, "js") == 0) {
+        return "Content-Type: application/javascript\r\n\r\n";
+    }
+    return 0;
+}
+
+void send_file(char *file, int file_len, int sockfd) {
 }
